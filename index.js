b07accbc7bcabbdaa9e31ea4be23b5b91827046a
xzlash <root@localhost>
2017-09-28 02:23:36 -0400

    rollback callback addition

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const Request = require("./request");
 const http = require("http");
 
 class Xzhttp {
-    constructor(call) {
+    constructor() {
         this.links = {};
-        if (typeof (call) == 'function') {
-            this.call = call;
-        } else {
-            this.call = function () {void 0}
-        }
     }
     
     link(path, func) {
@@ -21,11 +16,9 @@ class Xzhttp {
         
         if (App.links[Reqh.path]) {
             //if it is registered
-            this.call(200, Reqh);
             App.links[Reqh.path](Reqh);
         } else {
             //404
-            this.call(404, Reqh);
             res.statusCode = 404;
             res.write("404 Not Found");
             res.end();    
